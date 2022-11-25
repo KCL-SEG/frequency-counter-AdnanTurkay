e0c7f3a79a5678a95bae295cbcf9ee6eba777aa0
AdnanTurkay <adntrky@gmail.com>
2022-11-25 02:11:49 +0000

    Initial solution for "Frequency counter"

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,14 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    for i in items:
+        if str(i) not in frequencies:
+            frequencies[str(i)] = 1
+        else:
+            frequencies[str(i)] += 1
+
     return frequencies
